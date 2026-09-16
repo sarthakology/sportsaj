a42393c440ab14f1ffe2a0760cca24cae34c6079
sarthakology <thesarthakchauhan@gmail.com>
2026-09-16 21:10:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
-import Partners from './pages/Partners';
 import OurJourney from './pages/OurJourney';
-import Team from './pages/Team';
 import Contact from './pages/Contact';
 import BuyNow from './pages/BuyNow';
 import Ahemdabad from './pages/Ahemdabad';
@@ -18,9 +16,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/partners" element={<Partners />} />
           <Route path="/our-journey" element={<OurJourney />} />
-          <Route path="/team" element={<Team />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/buy-now" element={<BuyNow />} />
           <Route path="/ahemdabad" element={<Ahemdabad />} />
